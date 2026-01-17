3210859ef80dc70d00df2a6f02e7c1c05e31076f
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2026-01-17 09:04:36 +0530

    Update miles_to_km.py

--- miles_to_km.py
+++ miles_to_km.py
@@ -1,8 +1,9 @@
 import sys
-if sys.argv:
-    miles=float(input("Enter distance in miles:"))
-else:
-    miles=4
 
-result=miles*1.607
-print("Miles to kilometer converted value is:",result)
+if len(sys.argv) > 2:
+    miles = float(sys.argv[1])
+else:
+    miles = 4
+
+result = miles * 1.609
+print("Miles to kilometer converted value is:", result)
